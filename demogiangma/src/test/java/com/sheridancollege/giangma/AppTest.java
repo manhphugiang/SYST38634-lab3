c9fad8570bb8c261c7f5aeb3cb52d726273c85be
manhphugiang <122310965+manhphugiang@users.noreply.github.com>
2025-03-02 14:37:21 -0500

    task1

---
+++ demogiangma/src/test/java/com/sheridancollege/giangma/AppTest.java
@@ -0,0 +1,20 @@
+package com.sheridancollege.giangma;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
